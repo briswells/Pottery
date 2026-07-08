0b63f190847e728190fd79d08f2bac7213b4e33a
briswells <briswells@gmail.com>
2026-07-08 12:26:50 -0700

    fix(admin): include S3 storage client in importMap regardless of build-time env

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -3,6 +3,7 @@ import { PriceField as PriceField_72771f0a201d8559f61235df937c801f } from '../..
 import { ClassInstanceAutofill as ClassInstanceAutofill_b13cbfd49157f97f30d6b40dd1f0e704 } from '../../../admin/ClassInstanceAutofill'
 import { default as default_22888f2c0bc4c65baa7e4a6dffcdfbce } from '../../../admin/MembersNavLink'
 import { default as default_06a54c1e10226f174f5553d86160e206 } from '../../../admin/MyClassesNavLink'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 import { default as default_cf537fe5b5184f117c20993b2cf0e347 } from '../../../admin/views/MyClasses'
 import { default as default_d115c6c8ac0c7d6e6b2012866f1b7660 } from '../../../admin/views/MyClassRoster'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
@@ -14,6 +15,7 @@ export const importMap = {
   "/admin/ClassInstanceAutofill#ClassInstanceAutofill": ClassInstanceAutofill_b13cbfd49157f97f30d6b40dd1f0e704,
   "/admin/MembersNavLink#default": default_22888f2c0bc4c65baa7e4a6dffcdfbce,
   "/admin/MyClassesNavLink#default": default_06a54c1e10226f174f5553d86160e206,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
   "/admin/views/MyClasses#default": default_cf537fe5b5184f117c20993b2cf0e347,
   "/admin/views/MyClassRoster#default": default_d115c6c8ac0c7d6e6b2012866f1b7660,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
